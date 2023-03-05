b7bff954970948c488a06f622128875e2aae24c3
rdbo <rdbodev@gmail.com>
2023-03-05 18:51:02 -0300

    added font

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,7 +5,7 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
     <header class="sticky top-0">
             <nav class="flex font-bold bg-gray-100 items-center">
-                <RouterLink :to="{name: 'home'}" class="text-4xl px-2 drop-shadow-bold-sm flex items-center"><img class="w-16 mr-1" src="@/assets/img/apple.svg"/> <span class="text-primary-100">Nutri</span><span class="text-secondary-100">Now</span></RouterLink>
+                <RouterLink :to="{name: 'home'}" class="text-4xl px-2 drop-shadow-bold-sm flex items-center"><img class="w-16 mr-1" src="@/assets/imgs/apple.svg"/> <span class="text-primary-100">Nutri</span><span class="text-secondary-100">Now</span></RouterLink>
                 <div class="grow uppercase text-lg flex justify-center">
                     <RouterLink :to="{name: 'home'}" class="nav-item">Home</RouterLink>
                     <RouterLink :to="{name: 'about'}" class="nav-item">About</RouterLink>
