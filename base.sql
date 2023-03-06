3cfe548b6ed1d9d0ca05c9edba1ec255828eb8b3
rdbo <rdbodev@gmail.com>
2023-03-06 15:29:23 -0300

    updated sql base

--- base.sql
+++ base.sql
@@ -5,11 +5,27 @@ DROP DATABASE IF EXISTS nutrinow;
 CREATE DATABASE nutrinow;
 \c nutrinow;
 
+CREATE TABLE user_account (
+    id SERIAL,
+    name VARCHAR(100) NOT NULL,
+    gender CHAR(1) NOT NULL,
+    birthdate DATE NOT NULL,
+    PRIMARY KEY(id)
+);
+
 CREATE TABLE nutrient (
     id SERIAL,
     name VARCHAR(100) NOT NULL,
     unit VARCHAR(100) NOT NULL,
     PRIMARY KEY(id)
+);
+
+CREATE TABLE user_nutrition (
+    user_id SERIAL,
+    nutrient_id SERIAL,
+    daily_intake FLOAT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user_account(id),
+    FOREIGN KEY (nutrient_id) REFERENCES nutrient(id)
 );
 
 CREATE TABLE food (
@@ -34,6 +50,32 @@ CREATE TABLE serving_nutrient (
     calories FLOAT NOT NULL,
     FOREIGN KEY (serving_id) REFERENCES serving(id),
     FOREIGN KEY (nutrient_id) REFERENCES nutrient(id)
+);
+
+CREATE TABLE meal (
+    id SERIAL,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE meal_serving (
+    meal_id SERIAL,
+    serving_id SERIAL,
+    FOREIGN KEY (meal_id) REFERENCES meal(id),
+    FOREIGN KEY (serving_id) REFERENCES serving(id)
+);
+
+CREATE TABLE diet (
+    id SERIAL,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE diet_meal (
+    diet_id SERIAL,
+    meal_id SERIAL,
+    FOREIGN KEY (diet_id) REFERENCES diet(id),
+    FOREIGN KEY (meal_id) REFERENCES meal(id)
 );
 
 INSERT INTO nutrient(name, unit) VALUES
