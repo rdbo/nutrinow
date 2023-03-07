e9ea4da4c67b1595c8a7689312a8185c561764ac
rdbo <rdbodev@gmail.com>
2023-03-07 17:17:55 -0300

    added footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,19 +3,27 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-    <header class="sticky top-0">
-            <nav class="flex font-bold bg-gray-100 items-center flex-col md:flex-row">
-                <RouterLink :to="{name: 'home'}" class="text-4xl px-2 drop-shadow-bold-sm flex items-center"><img class="w-16 mr-1" src="@/assets/imgs/apple.svg"/> <span class="text-primary-100">Nutri</span><span class="text-secondary-100">Now</span></RouterLink>
-                <div class="grow uppercase text-lg flex justify-center">
-                    <RouterLink :to="{name: 'home'}" class="nav-item">Home</RouterLink>
-                    <RouterLink :to="{name: 'about'}" class="nav-item">About</RouterLink>
-                </div>
-            </nav>
-    </header>
+    <div class="min-h-screen">
+        <header class="sticky top-0">
+                <nav class="flex font-bold bg-gray-100 items-center flex-col md:flex-row">
+                    <RouterLink :to="{name: 'home'}" class="text-4xl px-2 drop-shadow-bold-sm flex items-center"><img class="w-16 mr-1" src="@/assets/imgs/apple.svg"/> <span class="text-primary-100">Nutri</span><span class="text-secondary-100">Now</span></RouterLink>
+                    <div class="grow uppercase text-lg flex justify-center">
+                        <RouterLink :to="{name: 'home'}" class="nav-item">Home</RouterLink>
+                        <RouterLink :to="{name: 'about'}" class="nav-item">About</RouterLink>
+                    </div>
+                </nav>
+        </header>
 
-    <main>
-        <RouterView/>
-    </main>
+        <main>
+            <RouterView/>
+        </main>
+    </div>
+
+    <footer class="py-4 bg-gray-300 flex flex-col justify-center items-center text-center">
+        <p>Copyright © Rdbo</p>
+        <p>All Rights Reserved</p>
+        <p>This website is licensed under the <a class="text-blue-500" href="https://www.gnu.org/licenses/agpl-3.0.en.html">GNU AGPLv3.0</a></p>
+    </footer>
 </template>
 
 <style scoped>
