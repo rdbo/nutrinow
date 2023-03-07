ad7d88293e83c7f63a9a798bdea062d0b64e479e
rdbo <rdbodev@gmail.com>
2023-03-07 17:10:10 -0300

    added welcome screen

---
+++ frontend/src/components/Welcome.vue
@@ -0,0 +1,26 @@
+<script setup>
+</script>
+
+<template>
+    <div class="py-8 flex flex-col justify-center items-center text-center">
+        <h1 class="text-6xl text-gray-800">Welcome to NutriNow</h1>
+        <h2 class="text-4xl text-gray-700">Make the diet of your dreams</h2>
+        <!--
+            File: healthy-foods.png
+            License: CC BY-NC 4.0 - https://creativecommons.org/licenses/by-nc/4.0/
+            Source: https://www.pngall.com/diet-png/download/53680
+        -->
+        <img src="@/assets/imgs/healthy-foods.png" class="w-96 py-4"/>
+        <button class="text-gray-800 border-2 border-gray-700 uppercase text-2xl bg-primary-100 px-4 py-4 rounded-md hover:bg-secondary-100">Get Started</button>
+    </div>
+</template>
+
+<style scoped>
+button {
+    transition: all 0.2s ease-in-out;
+}
+
+button:hover {
+    @apply bg-secondary-100 scale-125;
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,12 +1,10 @@
 <script setup>
+import Welcome from "../components/Welcome.vue";
 import FoodEditor from "../components/FoodEditor.vue";
 </script>
 
 <template>
-    <h1>Welcome to NutriNow</h1>
-    <h2>Make the diet of your dreams!</h2>
-    <FoodEditor :foodInfo="{ name: 'Apple' }"/>
-    <FoodEditor :foodInfo="null"/>
+    <Welcome/>
 </template>
 
 <style scoped>
