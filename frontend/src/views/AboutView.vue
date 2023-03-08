b0724adca03fcc188104bbae4e78e37fbf5af632
rdbo <rdbodev@gmail.com>
2023-03-08 06:18:36 -0300

    added about page

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -2,7 +2,20 @@
 </script>
 
 <template>
-    <h1>About Page</h1>
+    <div class="flex flex-col justify-center items-center text-center text-gray-700">
+        <h1 class="text-6xl my-4">About NutriNow</h1>
+        <p class="text-xl">
+            This is an open source project created by Rdbo with the purpose of being the best<br/>
+            website to make your own diet, taking in consideration all nutrients and minerals<br/>
+            required for humans to live.<br/>
+            The other options are either paid, or are just not good enough. This is why this<br/>
+            project started in the first place.<br/>
+            The source code is available on GitHub: <a href="https://github.com/rdbo/nutrinow" class="text-blue-500">https://github.com/rdbo/nutrinow</a><br/>
+            NutriNow is licensed under the GNU Affero General Public License version 3.0 (GNU AGPLv3.0).<br/>
+            More information about the license at: <a href="https://www.gnu.org/licenses/agpl-3.0.en.html" class="text-blue-500">https://www.gnu.org/licenses/agpl-3.0.en.html</a>
+        </p>
+        <img src="@/assets/imgs/apple.svg" class="w-80"/>
+    </div>
 </template>
 
 <style scoped>
