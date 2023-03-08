4a1eb66840ba128b3662a9a2f82d94cd38a37371
rdbo <rdbodev@gmail.com>
2023-03-07 21:52:55 -0300

    added 404 page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import AboutView from '../views/AboutView.vue';
 import LoginView from '../views/LoginView.vue';
+import PageNotFound from '../views/PageNotFound.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
             path: '/login',
             name: 'login',
             component: LoginView
+        },
+        {
+            path: '/:invalidPage(.*)',
+            name: 'not_found',
+            component: PageNotFound
         }
     ]
 });
---
+++ frontend/src/views/PageNotFound.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { useRouter } from 'vue-router';
+const router = useRouter();
+</script>
+
+<template>
+    <div class="flex flex-col justify-center items-center">
+        <h1 class="text-gray-700 text-6xl my-4">Page Not Found!</h1>
+        <button @click="router.push({ name: 'home' })" class="text-4xl bg-primary-100 px-4 py-4 rounded-lg border-2 border-gray-700 text-gray-700">Return to home page</button>
+    </div>
+</template>
+
+<style scoped>
+button {
+    transition: all 0.2s ease-in-out;
+}
+
+button:hover {
+    @apply bg-secondary-100;
+}
+</style>
