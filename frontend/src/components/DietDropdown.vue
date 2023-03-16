f57057581be067321a695735ec6f90be052d3781
rdbo <rdbodev@gmail.com>
2023-03-16 06:17:30 -0300

    made dropdown menu larger on smaller screens

--- frontend/src/components/DietDropdown.vue
+++ frontend/src/components/DietDropdown.vue
@@ -15,7 +15,7 @@ import { ChevronDownIcon } from '@heroicons/vue/20/solid'
         </div>
 
         <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-            <MenuItems class="absolute left-0 z-10 w-56 mt-2 origin-top-right rounded-md bg-white shadow-lg ring-2 ring-black ring-opacity-10 focus:outline-none">
+            <MenuItems class="absolute left-0 z-10 w-full md:w-56 mt-2 origin-top-right rounded-md bg-white shadow-lg ring-2 ring-black ring-opacity-10 focus:outline-none">
                 <div class="py-1">
                     <MenuItem v-slot="{ active }">
                         <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-lg']">Diet 2</a>
