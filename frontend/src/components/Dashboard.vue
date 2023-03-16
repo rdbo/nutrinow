07415888407b509225fff7ff1ace679379cc9ee5
rdbo <rdbodev@gmail.com>
2023-03-15 21:46:04 -0300

    added components for each food of a meal

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -1,7 +1,6 @@
 <script setup>
-import { TrashIcon } from "@heroicons/vue/20/solid";
-import { PencilSquareIcon } from "@heroicons/vue/20/solid";
 import DietDropdown from "./DietDropdown.vue";
+import MealFood from "./MealFood.vue";
 </script>
 
 <template>
@@ -15,43 +14,7 @@ import DietDropdown from "./DietDropdown.vue";
                 <div class="border-gray-700 border-2 rounded-lg flex flex-col bg-secondary-100">
                     <h2 class="text-xl text-center border-b-2 border-gray-700 py-2">Breakfest</h2>
                     <div>
-                        <div class="flex justify-between border-b-2 border-gray-700 bg-orange-200">
-                            <div class="flex">
-                                <!--
-                                    TODO: Remove or implement up/down on database
-
-                                <div class="flex flex-col bg-gray-200 border-r-2 border-gray-700">
-                                    <button class="grow border-b-2 border-gray-700 px-1">Up</button>
-                                    <button class="grow px-1">Down</button>
-                                </div>
-                                -->
-                                <button class="bg-gray-200 px-4 py-2 border-r-2 border-gray-700">
-                                    <PencilSquareIcon class="w-6"/>
-                                </button>
-                                <div class="flex items-center justify-center text-center">
-                                    <p class="text-lg my-2 mx-2">Chicken Breast (100g)</p>
-                                </div>
-                            </div>
-                            <div class="text-gray-500 flex">
-                                <div class="mx-2 my-2 flex">
-                                    <div class="mx-4 text-right">
-                                        <p>Calories:</p>
-                                        <p>Carbohydrates:</p>
-                                        <p>Proteins:</p>
-                                        <p>Fats:</p>
-                                    </div>
-                                    <div>
-                                        <p>400 kcal</p>
-                                        <p>5g</p>
-                                        <p>32g</p>
-                                        <p>15g</p>
-                                    </div>
-                                </div>
-                                <button class="bg-red-300 text-red-500 px-4 py-2 flex items-center justify-center border-l-2 border-gray-700 py-4 px-4">
-                                    <TrashIcon class="w-6"/>
-                                </button>
-                            </div>
-                        </div>
+                        <MealFood food="Chicken Breast" amount="100g" cals="400" carbs="12" prots="32" fats="18"/>
                     </div>
                     <div class="flex justify-center items-center">
                         <button class="text-center bg-red-300 px-8 py-4 my-2 border-gray-700 border-2 rounded-md text-xl">Add Food</button>
---
+++ frontend/src/components/MealFood.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { TrashIcon } from "@heroicons/vue/20/solid";
+import { PencilSquareIcon } from "@heroicons/vue/20/solid";
+
+const props = defineProps(["food", "amount", "cals", "carbs", "prots", "fats"]);
+</script>
+
+<template>
+<div class="flex justify-between border-b-2 border-gray-700 bg-orange-200">
+    <div class="flex">
+        <!--
+            TODO: Remove or implement up/down on database
+
+        <div class="flex flex-col bg-gray-200 border-r-2 border-gray-700">
+            <button class="grow border-b-2 border-gray-700 px-1">Up</button>
+            <button class="grow px-1">Down</button>
+        </div>
+        -->
+        <button class="bg-gray-200 px-4 py-2 border-r-2 border-gray-700">
+            <PencilSquareIcon class="w-6"/>
+        </button>
+        <div class="flex items-center justify-center text-center">
+            <p class="text-lg my-2 mx-2">{{ food }} ({{ amount }})</p>
+        </div>
+    </div>
+    <div class="text-gray-500 flex">
+        <div class="mx-2 my-2 flex">
+            <div class="mx-4 text-right">
+                <p>Calories:</p>
+                <p>Carbohydrates:</p>
+                <p>Proteins:</p>
+                <p>Fats:</p>
+            </div>
+            <div>
+                <p>{{ cals }}kcal</p>
+                <p>{{ carbs }}g</p>
+                <p>{{ prots }}g</p>
+                <p>{{ fats }}g</p>
+            </div>
+        </div>
+        <button class="bg-red-300 text-red-500 px-4 py-2 flex items-center justify-center border-l-2 border-gray-700 py-4 px-4">
+            <TrashIcon class="w-6"/>
+        </button>
+    </div>
+</div>
+</template>
