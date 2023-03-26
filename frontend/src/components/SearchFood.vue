6d5f1328b5e69f0fdcffc9c4b0e1f66949815901
rdbo <rdbodev@gmail.com>
2023-03-26 17:55:49 -0300

    improved search food result component

--- frontend/src/components/SearchFood.vue
+++ frontend/src/components/SearchFood.vue
@@ -7,7 +7,7 @@ const displayNutrients = getDisplayNutrients(props.food.servings[0].nutrients);
 </script>
 
 <template>
-<div class="flex justify-between border-b-2 border-gray-700 bg-gray-200">
+<div class="flex justify-between border-b-2 border-gray-700 bg-gray-200 cursor-pointer select-none">
     <div class="flex">
         <!--
             TODO: Remove or implement up/down on database
@@ -21,7 +21,7 @@ const displayNutrients = getDisplayNutrients(props.food.servings[0].nutrients);
             <p class="text-lg my-2 mx-2">{{ food.name }} ({{ food.servings[0].amount }}{{ food.servings[0].unit }})</p>
         </div>
     </div>
-    <div class="text-gray-500 flex text-base cursor-pointer select-none">
+    <div class="text-gray-500 flex text-base">
         <div class="mx-2 my-2 flex">
             <div class="mx-4 text-right">
                 <div v-for="nutrient in displayNutrients">
