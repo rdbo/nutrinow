5ad816167a7972bd8944483e0601ff700366a346
rdbo <rdbodev@gmail.com>
2023-03-30 12:26:45 -0300

    absolute navbar items on mobile

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -33,15 +33,18 @@ setInterval(updateSession, 100);
 <template>
     <div class="min-h-screen flex flex-col">
         <header class="sticky top-0 z-50">
-	    <nav class="bg-gray-100 border-gray-200">
+            <!-- click overlay for mobile -->
+            <div @click="showNavItems = false" id="nav_overlay" :class="{ 'opacity-40': showNavItems, 'hidden': !showNavItems }" class="opacity-0 md:hidden absolute top-0 h-screen w-screen bg-black"></div>
+
+	    <nav class="relative bg-gray-100 border-gray-200 md:static">
 		<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto md:flex-col">
                     <div class="flex flex-wrap items-center justify-between w-full mx-auto md:items-stretch md:p-0 md:flex-row">
                         <RouterLink :to="{ name: 'home' }" class="text-4xl p-2 drop-shadow-bold-sm flex items-center md:flex-col md:justify-center"><div class="flex"><img class="w-16 mr-1" src="@/assets/imgs/apple.svg"/> <span class="text-primary-100 flex items-center">Nutri</span><span class="text-secondary-100 flex items-center">Now</span></div></RouterLink>
                         <button @click="showNavItems = !showNavItems" data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 mr-4 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
                             <Bars3Icon class="w-6 h-6"/>
                         </button>
-                        <div :class="{ 'hidden': !showNavItems }" class="w-full md:flex md:w-auto text-lg md:flex md:flex-col" id="navbar-default">
-                            <ul class="font-medium flex flex-col p-4 md:p-0 md:px-4 mt-4 border border-gray-100 rounded-lg bg-white md:flex-row md:space-x-4 md:mt-0 md:border-0 md:bg-gray-100 md:grow">
+                        <div :class="{ 'hidden': !showNavItems }" class="p-4 bg-gray-100 w-full absolute top-full md:p-0 md:static md:flex md:w-auto text-lg md:flex md:flex-col" id="navbar-default">
+                            <ul class="font-medium flex flex-col p-4 md:p-0 md:px-4 border border-gray-100 rounded-lg bg-white md:flex-row md:space-x-4 md:mt-0 md:border-0 md:bg-gray-100 md:grow">
                                 <li>
                                     <RouterLink :session_id="test" :to="{ name: 'home' }" class="nav-item" aria-current="page">Home</RouterLink>
                                 </li>
@@ -95,5 +98,9 @@ li {
 
 .nav-item.router-link-active {
     @apply bg-lime-400 text-gray-800 md:bg-transparent md:text-lime-500 md:border-lime-400;
+}
+
+#nav_overlay {
+    transition: 0.2 all ease-in-out;
 }
 </style>
