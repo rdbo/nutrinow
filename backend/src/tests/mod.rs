5e2873da36b2d3fa381d505fd6497673e6c16e66
rdbo <rdbodev@gmail.com>
2023-06-24 10:05:43 -0300

    implemented start server test

--- backend/src/tests/mod.rs
+++ backend/src/tests/mod.rs
@@ -1,19 +1,24 @@
 #[cfg(test)]
 mod tests {
-    use crate::{create_app, create_db, main, settings::Settings};
-    use actix_web::{
-        http::{self, header::ContentType},
-        test,
-    };
+    use crate::{create_app, create_db, settings::Settings};
+    use actix_web::test;
     use anyhow::Result;
+    use env_logger;
+    use log::info;
 
     #[actix_web::test]
     async fn test_start_server() -> Result<()> {
+        env_logger::init();
+        info!("Initializing actix-web server");
         let settings = Settings::load()?;
+        info!("Settings: {:?}", settings);
         let dbpool = create_db(&settings).await?;
         let app = test::init_service(create_app(dbpool.clone())).await;
         let req = test::TestRequest::get().uri("/").to_request();
-        test::call_and_read_body(&app, req).await;
+        info!("Sending GET request to /");
+        let resp = test::call_service(&app, req).await;
+        info!("Response: {}", resp.status());
+        assert!(resp.status().is_success());
         Ok(())
     }
 }
